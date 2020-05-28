af57621ed5a853adaf9b7c39b3762fe50c9559e2
Animenosekai <maxence.osawa@gmail.com>
2020-05-28 20:07:14 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'fileinfo',         # How you named your package folder (MyLib)
   packages = ['fileinfo'],   # Chose the same as "name"
-  version = '1.2.2',      # Start with a small number and increase it with every change you make
+  version = '1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A file? An info!\n Get infos on file with a single line of code!\n Works with a database of file extensions so that it can even give you the type of the file.',   # Give a short description about your library
   author = 'Anime no Sekai',                   # Type in your name
   author_email = 'niichannomail@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/Animenosekai/file_info.py',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Animenosekai/file_info/archive/v1.2.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Animenosekai/file_info/archive/v1.3.tar.gz',    # Archive link
   keywords = ['file', 'information', 'fileinfo', 'type', 'extension', 'file management'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
