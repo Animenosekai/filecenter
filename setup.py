611f3661af00e9eed881a96645b43bcd4723702b
Animenosekai <maxence.osawa@gmail.com>
2020-05-29 14:34:11 +0200

    new setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'filecenter',         # How you named your package folder (MyLib)
   packages = ['filecenter'],   # Chose the same as "name"
-  version = '1.3',      # Start with a small number and increase it with every change you make
+  version = '1.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A file? An info!\n Get infos on file with a single line of code!\n Works with a database of file extensions so that it can even give you the type of the file.',   # Give a short description about your library
   author = 'Anime no Sekai',                   # Type in your name
   author_email = 'niichannomail@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/Animenosekai/file_info.py',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Animenosekai/filecenter/archive/v1.3.1.tar.gz',    # Archive link
+  download_url = 'https://github.com/Animenosekai/filecenter/archive/v1.5.tar.gz',    # Archive link
   keywords = ['file', 'information', 'fileinfo', 'type', 'extension', 'file management'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
@@ -24,4 +32,6 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
   ],
+  long_description=long_description,
+  long_description_content_type='text/markdown'
 )
